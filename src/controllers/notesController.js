fd9ebfd13c98bd1b789a7cc040617117812dc8c3
Ocheretko Oleh <117608037+Ocheretko1992@users.noreply.github.com>
2026-01-27 05:08:34 +0200

    to fix get all notes

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -13,13 +13,13 @@ export const getAllNotes = async (req, res) => {
   }
 
   if (search) {
-    // notesQuery.where({$text:{$search: search}});
-    notesQuery.where({
-      $or: [
-        { title: { $regex: search, $options: 'i' } },
-        { content: { $regex: search, $options: 'i' } },
-      ],
-    });
+    notesQuery.where({$text:{$search: search}});
+    // notesQuery.where({
+    //   $or: [
+    //     { title: { $regex: search, $options: 'i' } },
+    //     { content: { $regex: search, $options: 'i' } },
+    //   ],
+    // });
   }
 
   const [totalNotes, notes] = await Promise.all([
