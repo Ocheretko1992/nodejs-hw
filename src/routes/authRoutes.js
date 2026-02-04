cf5e5dca85423e2943c5785ea16a92dd1312286b
Ocheretko Oleh <117608037+Ocheretko1992@users.noreply.github.com>
2026-02-04 06:36:28 +0200

    add reset password

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,12 @@
 import { celebrate } from "celebrate";
 import { Router } from "express";
-import { loginUserSchema, registerUserSchema } from "../validations/authValidation.js";
-import { loginUser, logoutUser, refreshUserSession, registerUser } from "../controllers/authController.js";
+import {
+  loginUserSchema,
+  registerUserSchema,
+  requestResetEmailSchema,
+  resetPasswordSchema,
+} from '../validations/authValidation.js';
+import { requestResetEmail, loginUser, logoutUser, refreshUserSession, registerUser, resetPassword } from "../controllers/authController.js";
 
 const router = Router();
 
@@ -9,6 +14,12 @@ router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/logout', logoutUser);
 router.post('/auth/refresh', refreshUserSession);
+
+router.post('/auth/request-reset-email',celebrate(requestResetEmailSchema),requestResetEmail);
+router.post(
+  '/auth/reset-password',celebrate(resetPasswordSchema),resetPassword);
+
+
 
 
 export default router;
