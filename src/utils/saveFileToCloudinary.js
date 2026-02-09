691bd0e9a35fce2829fee38592a637dc5d2f4b9b
Ocheretko Oleh <117608037+Ocheretko1992@users.noreply.github.com>
2026-02-10 01:29:34 +0200

    to fix my

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -8,14 +8,15 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export const saveFileToCloudinary = async (buffer, userId) => {
+export const saveFileToCloudinary = async (buffer) => {
   return new Promise((resolve, reject) => {
     const uploadStream = cloudinary.uploader.upload_stream(
       {
         folder: 'notes-app/avatars',
         resource_type: 'image',
-        public_id: `avatar_${userId}`,
         overwrite: true,
+        use_filename: true,
+        unique_filename:false,
       },
       (err, result) => {
         err ? reject(err) : resolve(result);
